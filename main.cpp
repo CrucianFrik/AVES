78d58ba55ce6bbdfd5c91d47cf197b2b99c07405
CrucianFrik <kokhanova.va@phystech.edu>
2022-08-12 16:29:14 +0300

    случайно слила в main ветку с файлами, в этом коммите файлы удалены

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "GPS_Path.h"
-#include <iostream>
-
-Log logs{};
-
-int main() {
-    logs.init("" + add_time("LOG") + ".txt");
-    GPS_Path GPS{"", "GPS_text.txt", PathType::curve, 5, 20, 5, 20};
-}
